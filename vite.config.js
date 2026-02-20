1fd80e574ca15baeec3e7a52a707bb2e92c5d660
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-20 11:07:40 -0500

    chore(pwa): disable PWA in dev environment

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true,
+        enabled: false,
         type: 'module',
       },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'sounds/*.mp3'],
